67e3e77c79d58b4766658896c8b984b9f8e05187
TR Theologides Rodriguez <atheologidesrodriguez@gmail.com>
2022-02-06 22:00:40 -0500

    UI and Symbols Updated
    Not yet complete, but almost

---
+++ Assets/Scripts/WeaponSwapPanel.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwapPanel : MonoBehaviour
+{
+    public Weapon unequippedWeapon;
+
+    public string unequippedWeaponCode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void UpdateSwapPanel()
+    {
+        
+    }
+}
