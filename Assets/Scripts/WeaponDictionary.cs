2e75b71a3b4d4f035f02a49564b65167c20a785d
TR Theologides Rodriguez <atheologidesrodriguez@gmail.com>
2022-02-26 11:11:49 -0500

    Dialogue stuff and tiles

---
+++ Assets/Scripts/WeaponDictionary.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponDictionary : MonoBehaviour
+{
+    public Dictionary<int, Weapon> SimpleWeapons;
+    public Dictionary<>
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
